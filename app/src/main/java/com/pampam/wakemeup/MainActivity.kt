64b0b44d8d357746f452b744ce111c28be7c8e07
Evist0 <picxonicman@yandex.ru>
2020-08-14 01:45:18 +0700

    Main Activity

--- app/src/main/java/com/pampam/wakemeup/MainActivity.kt
+++ app/src/main/java/com/pampam/wakemeup/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.pampam.wakemeup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        MapKitFactory.setApiKey("51764e68-c17a-4d63-8f9b-2cec05ceb2d0");
+        MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main)
+
+        mapview.map.move(
+            CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 0F),
+            null
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapview.onStop()
+        MapKitFactory.getInstance().onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapview.onStart()
+        MapKitFactory.getInstance().onStart()
     }
 }
