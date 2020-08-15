7edc86d2ad3aa3fd144617103494b3ef3f49be91
Evist0 <picxonicman@yandex.ru>
2020-08-15 15:58:55 +0700

    Current Location Feature

--- app/src/main/java/com/pampam/wakemeup/MainActivity.kt
+++ app/src/main/java/com/pampam/wakemeup/MainActivity.kt
@@ -1,31 +1,104 @@
 package com.pampam.wakemeup
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private var permissionRequestCode = 0
+
     private lateinit var mMap: GoogleMap
+    private lateinit var locationMarker: Marker
+
+    private lateinit var locationManager: LocationManager
+    private var locationListener: LocationListener = object : LocationListener {
+
+        override fun onLocationChanged(location: Location) {
+            val newLatLng = LatLng(location.latitude, location.longitude)
+
+            locationMarker.position = newLatLng
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(newLatLng))
+
+            val msg = "New Latitude: " + location.latitude + "New Longitude: " + location.longitude
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_LONG).show()
+        }
+
+        override fun onStatusChanged(
+            provider: String,
+            status: Int,
+            extras: Bundle
+        ) {
+        }
+
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    2000,
+                    1.0f,
+                    locationListener
+                )
+
+                val lastLocation =
+                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
+                if (lastLocation != null) {
+                    locationMarker = mMap.addMarker(
+                        MarkerOptions().title("Your position")
+                            .position(LatLng(lastLocation.latitude, lastLocation.longitude))
+                    )
+                }
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                Toast.makeText(this, "Нам необходимо знать, когда вас будить.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            else -> {
+                requestPermissions(
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    permissionRequestCode
+                )
+            }
+        }
     }
+
 }
